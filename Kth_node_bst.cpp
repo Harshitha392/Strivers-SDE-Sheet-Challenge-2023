fa61a87952ca29b55b6e5068b6e95b5022905c04
Harshitha Majety <82963311+Harshitha392@users.noreply.github.com>
2023-07-04 14:45:04 +0530

    Create Kth_node_bst.cpp

---
+++ Kth_node_bst.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+void solve(TreeNode<int> *root,int &k,int &ans){
+    if(root==NULL) return ;
+    solve(root->right,k,ans);
+    k--;
+    if(k==0) {
+        ans=root->data;
+        return ;
+    }
+    solve(root->left,k,ans);
+}
+int KthLargestNumber(TreeNode<int>* root, int k) 
+{
+    int ans=-1;
+    solve(root,k,ans);
+    return ans;
+}
