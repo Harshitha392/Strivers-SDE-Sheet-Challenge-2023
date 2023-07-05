3490c64aadb796edd65d1b9af7da62b9a975ee2c
Harshitha Majety <82963311+Harshitha392@users.noreply.github.com>
2023-07-05 11:38:53 +0530

    Create Two_Sum.cpp

---
+++ Two_Sum.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+/**********************************************************
+
+    Following is the Binary Tree Node structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+        public: 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BinaryTreeNode() {
+            if (left)
+                delete left;
+            if (right)
+                delete right;
+        }
+    };
+***********************************************************/
+void inorder(BinaryTreeNode<int>*root,vector<int>&v){
+    if(root==NULL)
+    return ;
+    inorder(root->left,v);
+    v.push_back(root->data);
+    inorder(root->right,v);
+}
+bool pairSumBst(BinaryTreeNode<int> *root, int k)
+{
+    vector<int>v ;
+     inorder(root,v);
+     int n=v.size();
+    int i=0;
+    int j=n-1;
+    while(i<n && j>=0){
+        if(v[i]+v[j]==k)
+        return true;
+        if(v[i]+v[j]>k)
+        j--;
+        else
+        i++;
+    }
+    return false;
+}
